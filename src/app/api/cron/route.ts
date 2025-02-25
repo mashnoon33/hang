942b9102c43133edeeba15e64a73527f720839c6
Mash <mashnoon33@gmail.com>
2025-02-25 15:03:55 -0500

    resend test

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,4 +1,22 @@
-export function GET(request: Request) {
+import { env } from '@/env';
+import { Resend } from 'resend';
+
+
+const resend = new Resend(env.AUTH_RESEND_KEY);
+
+export async function GET(request: Request) {
     console.log("cron job ran")
-    return new Response('Hello from Vercel!');
-  }
+    const { data, error } = await resend.emails.send({
+        from: 'Mash <test@mash.studio>',
+        to: ['mashnyy@gmail.com'],
+        subject: 'Hello World',
+        html: '<strong>It works!</strong>',
+    });
+
+    if (error) {
+        return console.error({ error });
+    }
+
+    console.log({ data });
+    return new Response(JSON.stringify(data));
+}
