8e7303324024e200e2650b91e0dbcccbe6a27440
Mash <mashnoon33@gmail.com>
2025-02-27 10:45:35 -0500

    fi8x

--- src/app/calendar/[slug]/layout.tsx
+++ src/app/calendar/[slug]/layout.tsx
@@ -10,16 +10,16 @@ export default function CalendarLayout({
 }) {
   return (
     <CalendarProvider>
-      <div className="flex h-full w-full">
+      <div className="flex h-screen w-full">
         {/* Side Drawer */}
-        <div className="w-[20rem]  border-r border-gray-200 min-w-[350px] hidden lg:block">
+        <div className="w-[20rem]  border-r border-gray-200 min-w-[350px] h-screen  hidden lg:block">
           <SideDrawer />
         </div>
 
         {/* Main Content */}
         <div className="flex-1 flex flex-col w-[calc(100%-20rem)]  overflow-auto">
           {/* Calendar Content */}
-          <div className="flex-1 overflow-auto ">
+          <div className="flex-1 overflow-auto h-full">
             {children}
           </div>
         </div>
