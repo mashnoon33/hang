14329597a8ecef00964e3e84ffce94cff41fcf5b
Mash <mashnoon33@gmail.com>
2025-03-17 10:15:40 -0400

    lovely

--- src/app/calendar/[slug]/layout.tsx
+++ src/app/calendar/[slug]/layout.tsx
@@ -1,24 +1,12 @@
-"use client";
+import { Metadata } from 'next';
+import { CalendarLayoutClient } from './components/CalendarLayoutClient';
 
-import { SidebarProvider } from "@/components/ui/sidebar";
-import { CalendarProvider } from "./components/CalendarProvider";
-import { FloatingActionButtonBar } from "./components/fab";
-import { SideDrawer } from "./components/side-drawer";
+
+
 export default function CalendarLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
-  return (
-
-    <CalendarProvider>
-      <SidebarProvider >
-        <SideDrawer />
-        {children}
-        <FloatingActionButtonBar />
-
-      </SidebarProvider>
-    </CalendarProvider>
-  );
+  return <CalendarLayoutClient>{children}</CalendarLayoutClient>;
 } 
